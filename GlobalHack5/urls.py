54d0f1d746e28813218057b656e1474d177de178
JAKE_MACBOOK <JAKE_MACBOOK@10.4.247.195>
2015-09-12 14:28:52 -0500

    testing urls

--- GlobalHack5/urls.py
+++ GlobalHack5/urls.py
@@ -11,7 +11,6 @@ urlpatterns = patterns(
     url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
     url(r"^admin/", include(admin.site.urls)),
     url(r"^account/", include("account.urls")),
-    url(r"^backend/", include("account.urls")),
 )
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
